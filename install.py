c3b0433fe323c1c79d3c0f116f6a96be6d5f6698
MANU S PILLAI <manupillai308@gmail.com>
2018-10-27 09:07:34 +0530

    Update install.py

--- install.py
+++ install.py
@@ -8,6 +8,6 @@ dependencies = ['absl-py==0.5.0','altgraph==0.16.1','astor==0.7.1','backcall==0.
 'pandas==0.23.4','pandocfilters==1.4.2','parso==0.3.1','pefile==2018.8.8','pickleshare==0.7.5','post==1.0.2','prometheus-client==0.4.1','prompt-toolkit==2.0.6','protobuf==3.6.1','public==1.0.3','pycparser==2.19','Pygments==2.2.0',
 'PyInstaller==3.4','python-dateutil==2.7.3','pytz==2018.5','pywin32-ctypes==0.2.0','pywinpty==0.5.4','PyYAML==3.13','pyzmq==17.1.2','qtconsole==4.4.1','query-string==1.0.2','request==1.0.2','requests==2.19.1','scikit-learn==0.20.0',
 'scipy==1.1.0','Send2Trash==1.5.0','simplegeneric==0.8.1','six==1.11.0','sklearn==0.0','sounddevice==0.3.12','tensorboard==1.11.0','tensorflow==1.11.0','termcolor==1.1.0','terminado==0.8.1','testpath==0.4.2','tornado==5.1.1',
-'traitlets==4.3.2','urllib3==1.23','wcwidth==0.1.7','webencodings==0.5.1','Werkzeug==0.14.1','widgetsnbextension==3.4.2']
+'traitlets==4.3.2','urllib3==1.23','wcwidth==0.1.7','webencodings==0.5.1','Werkzeug==0.14.1','widgetsnbextension==3.4.2', 'librosa']
 for depends in dependencies:
 	os.system('pip install ' + depends)
